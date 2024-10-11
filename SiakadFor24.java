242d3ff6b081506883c4e7d25673e2d45a9538cd
vindayunita <vindasari7684@gmail.com>
2024-10-11 10:22:03 +0700

    vinda siakadfor2

--- SiakadFor24.java
+++ SiakadFor24.java
@@ -5,6 +5,7 @@ public class SiakadFor24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0,tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,9 +16,16 @@ public class SiakadFor24 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus ++;
+            } else {
+                tidakLulus ++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
 }
